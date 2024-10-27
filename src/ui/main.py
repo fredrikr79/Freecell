707d5777d2f5a61b25ce558455996a67f1612a30
fredrikr79 <fredrikrobertsen7@gmail.com>
2024-10-27 16:33:46 +0100

    feat: minimal working pygame display

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,5 +1,31 @@
+import pygame
+
+FPS = 60
+
+
 def main():
-    print("hello world")
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("green")
+
+        pygame.draw.circle(
+            screen,
+            "magenta",
+            pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2),
+            40,
+        )
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
