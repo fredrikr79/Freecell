c9e1b9e0590d48851f8eafb85bdc98ead1cfd468
fredrikr79 <fredrikrobertsen7@gmail.com>
2024-11-17 19:09:56 +0100

    feat: add `ui/colors.py`

---
+++ src/ui/colors.py
@@ -0,0 +1,7 @@
+import pygame
+
+BACKGROUND_COLOR = pygame.Color(140, 173, 167)  # cambridge blue
+RED_SUIT_COLOR = pygame.Color(233, 79, 55)  # cinnabar
+BLACK_SUIT_COLOR = pygame.Color(57, 62, 65)  # onyx
+TEXT_COLOR = pygame.Color(244, 202, 224)  # fairy tale
+BUTTON_COLOR = pygame.Color(248, 250, 144)  # mindaro
